1607a7f9ae2a427900f22def19de714c741a8fbb
lucaspiresc <lucascicutti1995@gmail.com>
2019-08-31 00:30:59 -0300

    inserindo funcionalidade de rotacao de reta

--- WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
@@ -30,17 +30,30 @@
         {
             this.imagem = new System.Windows.Forms.PictureBox();
             this.painel = new System.Windows.Forms.Panel();
-            this.btApagar = new System.Windows.Forms.Button();
-            this.btCor = new System.Windows.Forms.Button();
-            this.txtY = new System.Windows.Forms.TextBox();
+            this.btn_dda = new System.Windows.Forms.Button();
+            this.xFim = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.yFim = new System.Windows.Forms.TextBox();
+            this.xInit = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.txtX = new System.Windows.Forms.TextBox();
-            this.lbY = new System.Windows.Forms.Label();
-            this.lbX = new System.Windows.Forms.Label();
-            this.desenhar = new System.Windows.Forms.Button();
-            this.cdlg = new System.Windows.Forms.ColorDialog();
             this.btn_Translado = new System.Windows.Forms.Button();
+            this.lbX = new System.Windows.Forms.Label();
+            this.lbY = new System.Windows.Forms.Label();
+            this.txtY = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btApagar = new System.Windows.Forms.Button();
+            this.yInit = new System.Windows.Forms.TextBox();
+            this.btCor = new System.Windows.Forms.Button();
+            this.cdlg = new System.Windows.Forms.ColorDialog();
+            this.btn_Rotacionar = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txt_Graus = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.imagem)).BeginInit();
             this.painel.SuspendLayout();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // imagem
@@ -49,26 +62,159 @@
             this.imagem.Dock = System.Windows.Forms.DockStyle.Fill;
             this.imagem.Location = new System.Drawing.Point(0, 0);
             this.imagem.Name = "imagem";
-            this.imagem.Size = new System.Drawing.Size(784, 562);
+            this.imagem.Size = new System.Drawing.Size(946, 562);
             this.imagem.TabIndex = 0;
             this.imagem.TabStop = false;
-            this.imagem.MouseDown += new System.Windows.Forms.MouseEventHandler(this.imagem_Click);
+            this.imagem.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Imagem_Click);
             // 
             // painel
             // 
-            this.painel.Controls.Add(this.btn_Translado);
+            this.painel.Controls.Add(this.btn_dda);
+            this.painel.Controls.Add(this.xFim);
+            this.painel.Controls.Add(this.label3);
+            this.painel.Controls.Add(this.label4);
+            this.painel.Controls.Add(this.yFim);
+            this.painel.Controls.Add(this.xInit);
+            this.painel.Controls.Add(this.groupBox1);
+            this.painel.Controls.Add(this.label1);
+            this.painel.Controls.Add(this.label2);
             this.painel.Controls.Add(this.btApagar);
+            this.painel.Controls.Add(this.yInit);
             this.painel.Controls.Add(this.btCor);
-            this.painel.Controls.Add(this.txtY);
-            this.painel.Controls.Add(this.txtX);
-            this.painel.Controls.Add(this.lbY);
-            this.painel.Controls.Add(this.lbX);
-            this.painel.Controls.Add(this.desenhar);
             this.painel.Dock = System.Windows.Forms.DockStyle.Right;
-            this.painel.Location = new System.Drawing.Point(654, 0);
+            this.painel.Location = new System.Drawing.Point(702, 0);
             this.painel.Name = "painel";
-            this.painel.Size = new System.Drawing.Size(130, 562);
+            this.painel.Size = new System.Drawing.Size(244, 562);
             this.painel.TabIndex = 1;
+            // 
+            // btn_dda
+            // 
+            this.btn_dda.Location = new System.Drawing.Point(29, 330);
+            this.btn_dda.Name = "btn_dda";
+            this.btn_dda.Size = new System.Drawing.Size(75, 23);
+            this.btn_dda.TabIndex = 8;
+            this.btn_dda.Text = "DDA";
+            this.btn_dda.UseVisualStyleBackColor = true;
+            this.btn_dda.Click += new System.EventHandler(this.Btn_dda_Click);
+            // 
+            // xFim
+            // 
+            this.xFim.Enabled = false;
+            this.xFim.Location = new System.Drawing.Point(123, 245);
+            this.xFim.Name = "xFim";
+            this.xFim.Size = new System.Drawing.Size(68, 20);
+            this.xFim.TabIndex = 14;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(120, 228);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(39, 13);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "X Final";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(120, 268);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(39, 13);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Y Final";
+            // 
+            // yFim
+            // 
+            this.yFim.Enabled = false;
+            this.yFim.Location = new System.Drawing.Point(123, 284);
+            this.yFim.Name = "yFim";
+            this.yFim.Size = new System.Drawing.Size(68, 20);
+            this.yFim.TabIndex = 15;
+            // 
+            // xInit
+            // 
+            this.xInit.Enabled = false;
+            this.xInit.Location = new System.Drawing.Point(29, 245);
+            this.xInit.Name = "xInit";
+            this.xInit.Size = new System.Drawing.Size(68, 20);
+            this.xInit.TabIndex = 10;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.txt_Graus);
+            this.groupBox1.Controls.Add(this.btn_Rotacionar);
+            this.groupBox1.Controls.Add(this.txtX);
+            this.groupBox1.Controls.Add(this.btn_Translado);
+            this.groupBox1.Controls.Add(this.lbX);
+            this.groupBox1.Controls.Add(this.lbY);
+            this.groupBox1.Controls.Add(this.txtY);
+            this.groupBox1.Location = new System.Drawing.Point(22, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 184);
+            this.groupBox1.TabIndex = 8;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Transformações Geométricas";
+            // 
+            // txtX
+            // 
+            this.txtX.Location = new System.Drawing.Point(7, 32);
+            this.txtX.Name = "txtX";
+            this.txtX.Size = new System.Drawing.Size(68, 20);
+            this.txtX.TabIndex = 3;
+            // 
+            // btn_Translado
+            // 
+            this.btn_Translado.Location = new System.Drawing.Point(101, 29);
+            this.btn_Translado.Name = "btn_Translado";
+            this.btn_Translado.Size = new System.Drawing.Size(75, 23);
+            this.btn_Translado.TabIndex = 7;
+            this.btn_Translado.Text = "Transladar";
+            this.btn_Translado.UseVisualStyleBackColor = true;
+            this.btn_Translado.Click += new System.EventHandler(this.Btn_Translado_Click);
+            // 
+            // lbX
+            // 
+            this.lbX.AutoSize = true;
+            this.lbX.Location = new System.Drawing.Point(4, 15);
+            this.lbX.Name = "lbX";
+            this.lbX.Size = new System.Drawing.Size(14, 13);
+            this.lbX.TabIndex = 1;
+            this.lbX.Text = "X";
+            // 
+            // lbY
+            // 
+            this.lbY.AutoSize = true;
+            this.lbY.Location = new System.Drawing.Point(4, 55);
+            this.lbY.Name = "lbY";
+            this.lbY.Size = new System.Drawing.Size(14, 13);
+            this.lbY.TabIndex = 2;
+            this.lbY.Text = "Y";
+            // 
+            // txtY
+            // 
+            this.txtY.Location = new System.Drawing.Point(7, 71);
+            this.txtY.Name = "txtY";
+            this.txtY.Size = new System.Drawing.Size(68, 20);
+            this.txtY.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, 228);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "X Inicial";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(26, 268);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Y Inicial";
             // 
             // btApagar
             // 
@@ -78,75 +224,57 @@
             this.btApagar.TabIndex = 6;
             this.btApagar.Text = "Apagar";
             this.btApagar.UseVisualStyleBackColor = true;
-            this.btApagar.Click += new System.EventHandler(this.btApagar_Click);
+            this.btApagar.Click += new System.EventHandler(this.BtApagar_Click);
+            // 
+            // yInit
+            // 
+            this.yInit.Enabled = false;
+            this.yInit.Location = new System.Drawing.Point(29, 284);
+            this.yInit.Name = "yInit";
+            this.yInit.Size = new System.Drawing.Size(68, 20);
+            this.yInit.TabIndex = 11;
             // 
             // btCor
             // 
-            this.btCor.Location = new System.Drawing.Point(22, 485);
+            this.btCor.Location = new System.Drawing.Point(123, 527);
             this.btCor.Name = "btCor";
             this.btCor.Size = new System.Drawing.Size(75, 23);
             this.btCor.TabIndex = 5;
             this.btCor.Text = "Cor";
             this.btCor.UseVisualStyleBackColor = true;
-            this.btCor.Click += new System.EventHandler(this.btCor_Click);
+            this.btCor.Click += new System.EventHandler(this.BtCor_Click);
             // 
-            // txtY
+            // btn_Rotacionar
             // 
-            this.txtY.Location = new System.Drawing.Point(29, 85);
-            this.txtY.Name = "txtY";
-            this.txtY.Size = new System.Drawing.Size(68, 20);
-            this.txtY.TabIndex = 4;
+            this.btn_Rotacionar.Location = new System.Drawing.Point(101, 142);
+            this.btn_Rotacionar.Name = "btn_Rotacionar";
+            this.btn_Rotacionar.Size = new System.Drawing.Size(75, 23);
+            this.btn_Rotacionar.TabIndex = 8;
+            this.btn_Rotacionar.Text = "Rotacionar";
+            this.btn_Rotacionar.UseVisualStyleBackColor = true;
+            this.btn_Rotacionar.Click += new System.EventHandler(this.Btn_Rotacionar_Click);
             // 
-            // txtX
+            // label5
             // 
-            this.txtX.Location = new System.Drawing.Point(29, 46);
-            this.txtX.Name = "txtX";
-            this.txtX.Size = new System.Drawing.Size(68, 20);
-            this.txtX.TabIndex = 3;
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(4, 129);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(35, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Graus";
             // 
-            // lbY
+            // txt_Graus
             // 
-            this.lbY.AutoSize = true;
-            this.lbY.Location = new System.Drawing.Point(26, 69);
-            this.lbY.Name = "lbY";
-            this.lbY.Size = new System.Drawing.Size(14, 13);
-            this.lbY.TabIndex = 2;
-            this.lbY.Text = "Y";
-            // 
-            // lbX
-            // 
-            this.lbX.AutoSize = true;
-            this.lbX.Location = new System.Drawing.Point(26, 29);
-            this.lbX.Name = "lbX";
-            this.lbX.Size = new System.Drawing.Size(14, 13);
-            this.lbX.TabIndex = 1;
-            this.lbX.Text = "X";
-            // 
-            // desenhar
-            // 
-            this.desenhar.Location = new System.Drawing.Point(29, 160);
-            this.desenhar.Name = "desenhar";
-            this.desenhar.Size = new System.Drawing.Size(75, 23);
-            this.desenhar.TabIndex = 0;
-            this.desenhar.Text = "Desenhar";
-            this.desenhar.UseVisualStyleBackColor = true;
-            this.desenhar.Click += new System.EventHandler(this.desenhar_Click);
-            // 
-            // btn_Translado
-            // 
-            this.btn_Translado.Location = new System.Drawing.Point(29, 121);
-            this.btn_Translado.Name = "btn_Translado";
-            this.btn_Translado.Size = new System.Drawing.Size(75, 23);
-            this.btn_Translado.TabIndex = 7;
-            this.btn_Translado.Text = "Transladar";
-            this.btn_Translado.UseVisualStyleBackColor = true;
-            this.btn_Translado.Click += new System.EventHandler(this.Btn_Translado_Click);
+            this.txt_Graus.Location = new System.Drawing.Point(7, 145);
+            this.txt_Graus.Name = "txt_Graus";
+            this.txt_Graus.Size = new System.Drawing.Size(68, 20);
+            this.txt_Graus.TabIndex = 10;
             // 
             // tela
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(784, 562);
+            this.ClientSize = new System.Drawing.Size(946, 562);
             this.Controls.Add(this.painel);
             this.Controls.Add(this.imagem);
             this.Name = "tela";
@@ -154,6 +282,8 @@
             ((System.ComponentModel.ISupportInitialize)(this.imagem)).EndInit();
             this.painel.ResumeLayout(false);
             this.painel.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -166,11 +296,23 @@
         private System.Windows.Forms.TextBox txtX;
         private System.Windows.Forms.Label lbY;
         private System.Windows.Forms.Label lbX;
-        private System.Windows.Forms.Button desenhar;
         private System.Windows.Forms.Button btApagar;
         private System.Windows.Forms.Button btCor;
         private System.Windows.Forms.ColorDialog cdlg;
         private System.Windows.Forms.Button btn_Translado;
+        private System.Windows.Forms.TextBox xFim;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox yFim;
+        private System.Windows.Forms.TextBox xInit;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox yInit;
+        private System.Windows.Forms.Button btn_dda;
+        private System.Windows.Forms.Button btn_Rotacionar;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txt_Graus;
     }
 }
 
--- WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -20,6 +20,14 @@ namespace WindowsFormsApplication1
         int? yInicial = null;
         int? yFinal = null;
 
+        //Coodenadas da ultima reta desenhada
+        //que sera utilizado para rotacionar uma reta
+        int? xInicialBuffer = null;
+        int? xFinalBuffer = null;
+
+        int? yInicialBuffer = null;
+        int? yFinalBuffer = null;
+
         public tela()
         {
             InitializeComponent();            
@@ -27,7 +35,134 @@ namespace WindowsFormsApplication1
             corPreenche = Color.Black;                       
         }
 
-        private void desenhar_Click(object sender, EventArgs e)
+        //Seta para nulo as coordenadas finais e iniciais depois de rodar algum algorítmo
+        private void LimparIndices()
+        {
+            xInicial = null;
+            xInit.Text = null;
+
+            yInicial = null;
+            yInit.Text = null;
+
+            xFinal = null;
+            xFim.Text = null;
+
+            yFinal = null;
+            yFim.Text = null;
+        }
+
+        //Salva as coordenadas atuais no buffer
+        private void SalvarBuffer(int? x1, int? y1, int? x2, int? y2)
+        {
+            xInicialBuffer = x1;
+            yInicialBuffer = y1;
+
+            xFinalBuffer = x2;
+            yFinalBuffer = y2;
+        }
+
+        #region Transformacoes Geométricas
+
+        //Aplica a translação desejada na ultima reta desenhada
+        public void Translacao(int x1, int y1, int x2, int y2, int xT, int yT)
+        {
+            if (!(xT == 0 & yT == 0))
+            {
+
+                // Apaga a reta atual 
+                ApagaReta(x1, y1, x2, y2);
+
+                //Insere a reta alterada
+                DDA(x1 + xT, y1 - yT, x2 + xT, y2 - yT, corPreenche);
+
+                //Atualiza os valores do buffer
+                SalvarBuffer(x1 + xT, y1 - yT, x2 + xT, y2 - yT);
+            }
+        }
+
+        //Aplica a rotação pelo ângulo desejado na ultima reta desenhada
+        public void Rotacao(int x1, int y1, int x2, int y2, double angulo)
+        {
+            double xfinal, yFinal;
+
+            //Apaga a reta atual para depois desenhar ela rotacionada
+            ApagaReta(x1, y1, x2, y2);
+
+            //Translada o ponto final para que a reta comece na origem 
+            x2 = x2 - x1;
+            y2 = y2 - y1;
+
+            //Calcula o deslocamento do ponto final pós-rotação
+            xfinal = x2 * Math.Cos(angulo) - y2 * Math.Sin(angulo);
+            yFinal = x2 * Math.Sin(angulo) + y2 * Math.Cos(angulo);
+
+            //Desfaz a translação do ponto final
+            x2 = Convert.ToInt32(xfinal) + x1;
+            y2 = Convert.ToInt32(yFinal) + y1;
+
+            //Traça a nova reta alterada
+            DDA(x1, y1, x2, y2, corPreenche);
+
+            //Atualiza os valores do buffer
+            SalvarBuffer(x1, y1, x2, y2);
+        }
+
+        #endregion
+
+        #region Retas
+
+        //Utiliza o algoritmo DDA para desenhar uma reta a partir de seus pontos inicial e final
+        private void DDA(int xInicial, int yInicial, int xFinal, int yFinal, Color cor)
+        {
+            double dx = xFinal - xInicial;
+            double dy = yFinal - yInicial;
+
+            int nPassos;
+
+            if (Math.Abs(dx) >= Math.Abs(dy))
+            {
+                nPassos = Convert.ToInt32(Math.Abs(dx));
+            }
+            else
+            {
+                nPassos = Convert.ToInt32(Math.Abs(dy));
+            }
+
+            double xAdd = dx / nPassos;
+            double yAdd = dy / nPassos;
+
+            double x = xInicial;
+            double y = yInicial;
+
+            for (int i = 0; i < nPassos; i++)
+            {
+                x += xAdd;
+                y += yAdd;
+
+                try
+                {
+                    areaDesenho.SetPixel((int)x, (int)y, cor);
+                }
+                catch (Exception e)
+                {
+                    
+                }
+            }
+            imagem.Image = areaDesenho;
+        }
+
+        //Apaga a reta selecionada
+        public void ApagaReta(int x1, int y1, int x2, int y2)
+        {
+            //Traça uma reta da cor do fundo para "excluir" a reta
+            DDA(x1, y1, x2, y2, Color.White);
+        }
+
+        #endregion
+
+        #region Eventos
+
+        private void Desenhar_Click(object sender, EventArgs e)
         {
             int x;
             int y;
@@ -42,7 +177,7 @@ namespace WindowsFormsApplication1
             }
         }
 
-        private void btCor_Click(object sender, EventArgs e)
+        private void BtCor_Click(object sender, EventArgs e)
         {
             DialogResult result = cdlg.ShowDialog();
             if (result == DialogResult.OK)
@@ -51,13 +186,16 @@ namespace WindowsFormsApplication1
             }
         }
 
-        private void btApagar_Click(object sender, EventArgs e)
+        private void BtApagar_Click(object sender, EventArgs e)
         {
             areaDesenho = new Bitmap(imagem.Size.Width, imagem.Size.Height);
             imagem.Image = areaDesenho;
+
+            //Limpa o buffer de reta
+            SalvarBuffer(null, null, null, null);
         }
 
-        private void imagem_Click(object sender, MouseEventArgs e)
+        private void Imagem_Click(object sender, MouseEventArgs e)
         {
             if (e.Button == MouseButtons.Left)
             {
@@ -69,6 +207,9 @@ namespace WindowsFormsApplication1
                 {
                     xInicial = x;
                     yInicial = y;
+
+                    xInit.Text = xInicial.ToString();
+                    yInit.Text = yInicial.ToString();
                 }
                 //se eles já estão setados, o usuário está selecionando os pontos finais
                 else
@@ -76,81 +217,75 @@ namespace WindowsFormsApplication1
                     xFinal = x;
                     yFinal = y;
 
-                    DDA();
-                    
-                    //Depois de desenhar a figura, reinicia os pontos final e inicial
-                    xInicial = null;
-                    yInicial = null;
-
-                    xFinal = null;
-                    yFinal = null;
+                    xFim.Text = xFinal.ToString();
+                    yFim.Text = yFinal.ToString();
                 }
-            }
-        }
-
-        private void Translacao(int xT, int yT)
-        {
-            Bitmap novaArea = new Bitmap(areaDesenho.Size.Width, areaDesenho.Size.Height);
-
-            for (int i = 0; i < areaDesenho.Size.Width; i++)
-            {
-                for(int j = 0; j < areaDesenho.Size.Height; j++)
-                {
-                    if (i + xT > 0 && i + xT < areaDesenho.Size.Width && j +yT > 0 && j + yT < areaDesenho.Size.Height)
-                    {
-                        novaArea.SetPixel(i + xT, j + yT, areaDesenho.GetPixel(i, j));
-                    }
-                }
-            }
-
-            areaDesenho = novaArea;
-            imagem.Image = areaDesenho;
-        }
-
-        private void DDA()
-        {
-            double dx = xFinal.Value - xInicial.Value;
-            double dy = yFinal.Value - yInicial.Value;
-
-            int nPassos;
-
-            if(Math.Abs(dx) >= Math.Abs(dy))
-            {
-                nPassos = Convert.ToInt32(Math.Abs(dx));
-            }
-            else
-            {
-                nPassos = Convert.ToInt32(Math.Abs(dy));
-            }
-
-            double xAdd = dx / nPassos;
-            double yAdd = dy / nPassos;
-
-            double x = xInicial.Value;
-            double y = yInicial.Value;
-
-            for (int i = 0; i < nPassos; i++)
-            {
-                x += xAdd;
-                y += yAdd;
-
-                areaDesenho.SetPixel((int)x, (int)y, corPreenche);
-                imagem.Image = areaDesenho;
             }
         }
 
         private void Btn_Translado_Click(object sender, EventArgs e)
         {
-            int x;
-            int y;
-            if (int.TryParse(txtX.Text, out x) && int.TryParse(txtY.Text, out y))
+            if (xInicialBuffer.HasValue && yInicialBuffer.HasValue && xFinalBuffer.HasValue && yFinalBuffer.HasValue)
             {
-                Translacao(x, y);
+                int x;
+                int y;
+                if (int.TryParse(txtX.Text, out x) && int.TryParse(txtY.Text, out y))
+                {
+                    Translacao(xInicialBuffer.Value, yInicialBuffer.Value, xFinalBuffer.Value, yFinalBuffer.Value, x, y);
+                }
+                else
+                {
+                    MessageBox.Show("Favor preencher as coordenadas X e Y com valores inteiros válidos", "Erro");
+                }
             }
             else
             {
-                MessageBox.Show("Favor preencher as coordenadas X e Y com valores inteiros válidos", "Erro");
+                MessageBox.Show("Não existe reta na imagem, favor inserir uma reta primeiro", "Erro");
             }
         }
+
+        private void Btn_Rotacionar_Click(object sender, EventArgs e)
+        {
+            if (xInicialBuffer.HasValue && yInicialBuffer.HasValue && xFinalBuffer.HasValue && yFinalBuffer.HasValue)
+            {
+                double graus;
+                if (double.TryParse(txt_Graus.Text, out graus))
+                {
+                    //Passa o valor para radiano
+                    graus = graus * Math.PI / 180;
+                    Rotacao(xInicialBuffer.Value, yInicialBuffer.Value, xFinalBuffer.Value, yFinalBuffer.Value, graus);
+                }
+                else
+                {
+                    MessageBox.Show("Favor inserir o angulo de rotação com um valor válido", "Erro");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Não existe reta na imagem, favor inserir uma reta primeiro", "Erro");
+            }
+        }
+
+        private void Btn_dda_Click(object sender, EventArgs e)
+        {
+            if (xInicial != null && xFinal != null && yInicial != null && yFinal != null) {
+                //Roda o algoritmo
+                DDA(xInicial.Value, yInicial.Value, xFinal.Value, yFinal.Value, corPreenche);
+
+                //Salva as coordendas como a ultima reta desenhada
+                SalvarBuffer(xInicial, yInicial, xFinal, yFinal);
+
+                //Limpa as coordenadas do painel
+                LimparIndices();
+            }
+            else
+            {
+                MessageBox.Show("Favor preencher as coordenadas inicial e final com valores inteiros válidos", "Erro");
+            }
+        }
+
+        #endregion
+
+
     }
 }
